4d534b45a2bfe9489c190c96ba5cb6c03dc5c8c5
ferotek <davidguo94@gmail.com>
2023-05-05 10:28:17 -0700

    Update modeling_detr.py

--- src/transformers/models/detr/modeling_detr.py
+++ src/transformers/models/detr/modeling_detr.py
@@ -60,6 +60,15 @@ DETR_PRETRAINED_MODEL_ARCHIVE_LIST = [
     # See all DETR models at https://huggingface.co/models?filter=detr
 ]
 
+import torch.distributed as dist
+
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    return True
+
 
 @dataclass
 class DetrDecoderOutput(BaseModelOutputWithCrossAttentions):
